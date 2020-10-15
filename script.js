be8072be2a2465200efb9301c3af42c0532789bf
Erik Isaksen <72092585+Erikisaksen93@users.noreply.github.com>
2020-10-15 19:03:00 +0200

    Fixed it
    fuckyeaaaboooooi

--- script.js
+++ script.js
@@ -4,15 +4,12 @@ let age = 0;
 var adress = "";
 var country = "";
 var storeInfo = [];
-let i = 0;
+let i;
 
 
 //Maybe loop?
 function getData() {
 
-    for (i = 0; i < storeInfo.length; i++) {
-    }
-    
 firstName = document.getElementById('fname').value;
 storeInfo.push(firstName);
 
@@ -28,13 +25,16 @@ storeInfo.push(adress);
 country = document.getElementById('country').value;
 storeInfo.push(country);
 
-//output will always be the same array[x]??
 
-document.getElementById('output_fname').innerHTML +=storeInfo[i] + ", ";
+//output will always be the same array[x]?? FIXED
+
+document.getElementById('output_fname').innerHTML +=storeInfo[0] + ", ";
 document.getElementById('output_lname').innerHTML += storeInfo[1] + ", ";
 document.getElementById('output_age').innerHTML += storeInfo[2] + ", ";
 document.getElementById('output_adress').innerHTML += storeInfo[3] + ", ";
 document.getElementById('output_countries').innerHTML += storeInfo[4] + ", ";
+
+storeInfo.length = 0;
 
 
 //Must be a better way to clear all the boxes.
